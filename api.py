048071ecdc4bc328e9fb941d023b9a85b40bf1c0
Herculino Trotta <flaips.2@gmail.com>
2020-12-16 13:13:17 -0300

    fix: Variação nas ultimas 24hrs sem formatação
    Ops...

--- api.py
+++ api.py
@@ -10,4 +10,8 @@ def get_price():
     usd_valor = json['ethereum']['usd']
     brl_24h = json['ethereum']['brl_24h_change']
     usd_24hr = json['ethereum']['usd_24h_change']
+
+    brl_24h = "{:+,.2%}".format(brl_24h).replace('.', ',')
+    usd_24hr = "{:+,.2%}".format(usd_24hr).replace('.', ',')
+
     return brl_valor, brl_24h, usd_valor, usd_24hr
