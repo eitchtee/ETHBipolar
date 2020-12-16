69d957d4cdc6bf600684be315f1ee622a73437c9
Herculino Trotta <flaips.2@gmail.com>
2020-12-16 12:52:37 -0300

    feat: Adiciona variação em 24h à chamada da API

--- api.py
+++ api.py
@@ -2,8 +2,13 @@ import requests
 
 
 def get_price():
-    url = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=brl,usd"
+    url = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=brl,usd&include_24hr_change=true"
 
     response = requests.request("get", url)
     json = response.json()
-    return json['ethereum']['brl'], json['ethereum']['usd']
+    print(json)
+    brl_valor = json['ethereum']['brl']
+    usd_valor = json['ethereum']['usd']
+    brl_24h = json['ethereum']['brl_24h_change']
+    usd_24hr = json['ethereum']['usd_24h_change']
+    return brl_valor, brl_24h, usd_valor, usd_24hr
